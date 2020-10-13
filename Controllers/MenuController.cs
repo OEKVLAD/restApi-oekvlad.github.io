4525a7fde3aa0753965e477d6bda2c3d59bc83f2
Vladyslav Omelchenko <vladysalvo@gmail.com>
2020-10-13 20:06:18 +0200

    [ feature ]: get main menu

---
+++ Controllers/MenuController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestApi.Database;
+
+namespace RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuController : ControllerBase
+    {
+        [HttpGet]
+        [Route("main")]
+        public string Get(string url)
+        {
+            var sql = new CreatorSql();
+            return sql.request("select " +
+                "CONCAT( " +
+                "CONCAT( " +
+                " '{\"title\":\"', m.title, '\"', " +
+                " ',\"url\":\"',  m.url, '\"', " +
+                "',\"type\":\"', m.type, '\"}' " +
+                " ) " +
+                ") as json " +
+                "from menu m " +
+                "where  m.menu_name = 'main_menu' " +
+                "order by m.sort ASC; ");
+        }
+    }
+}
